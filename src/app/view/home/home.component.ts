9536b59c2be59b193968042fca7727ea95fba3be
ztimson <zaktimson@gmail.com>
2024-01-08 13:31:37 -0500

    Updated styling, added contact info, & restaurant

--- src/app/view/home/home.component.ts
+++ src/app/view/home/home.component.ts
@@ -1,7 +1,13 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './home.component.html'
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+	public day!: number;
+
+	ngOnInit() {
+		this.day = new Date().getDay();
+	}
+}
